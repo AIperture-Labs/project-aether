80077b7bf4b1c5f320f82377137cf1a8fb01906e
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-12-31 15:16:54 +0100

    fix: update clang-format style to LLVM and clean up helloworld.cpp formatting

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,11 +1,9 @@
 // Copyright (c) 2025 AIperture-Labs <xavier.beheydt@gmail.com>
 // SPDX-License-Identifier: MIT
 
-
 #include <iostream>
 
-int main(void)
-{
+int main(void) {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
