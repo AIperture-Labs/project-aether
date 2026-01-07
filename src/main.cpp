ea6c73cbe3036f501a9eefc56a578a3fb8fa2c1d
Xavier Beheydt <xavier.beheydt@gmail.com>
2026-01-07 12:52:14 +0100

    tuto: 09_shader_modules.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 #include <limits>
 #include <stdexcept>
@@ -98,6 +99,7 @@ class HelloTriangleApplication
         createLogicalDevice();
         createSwapChain();
         createImagesViews();
+        createGraphicsPipeline();
     }
 
     void mainLoop()
@@ -378,6 +380,32 @@ class HelloTriangleApplication
         }
     }
 
+    void createGraphicsPipeline()
+    {
+        std::string filename   = "slang.spv";
+        auto        shaderCode = readFile(filename);
+
+#if defined(_DEBUG)
+        std::cout << "Shader Buffer Size(" << filename << "): " << shaderCode.size() * sizeof(char) << std::endl;
+#endif
+
+        vk::raii::ShaderModule shaderModule = createShaderModule(shaderCode);
+
+        vk::PipelineShaderStageCreateInfo vertShaderStageInfo{.stage  = vk::ShaderStageFlagBits::eVertex,
+                                                              .module = shaderModule,
+                                                              .pName  = "vertMain"};
+        vk::PipelineShaderStageCreateInfo fragShaderStageInfo{.stage  = vk::ShaderStageFlagBits::eFragment,
+                                                              .module = shaderModule,
+                                                              .pName  = "fragMain"};
+        vk::PipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+    }
+
+    [[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char> &code) const
+    {
+        vk::ShaderModuleCreateInfo createInfo{.codeSize = code.size() * sizeof(char),
+                                              .pCode    = reinterpret_cast<const uint32_t *>(code.data())};
+        vk::raii::ShaderModule     shaderModule{device, createInfo};
+        return shaderModule;
     }
 
     static uint32_t calculateMinImageCount(const vk::SurfaceCapabilitiesKHR &surfaceCapabilities)
@@ -470,6 +498,23 @@ class HelloTriangleApplication
         auto deviceFeatures   = physicalDevice.getFeatures();
 
         return (deviceProperties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu && deviceFeatures.geometryShader);
+    }
+
+    static std::vector<char> readFile(const std::string &filename)
+    {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error("failed to open file!");
+        }
+
+        std::vector<char> buffer(file.tellg());
+        file.seekg(0, std::ios::beg);
+        file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+        file.close();
+
+        return buffer;
     }
 
    public:
